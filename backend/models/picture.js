96d77866f9ae14233b538899aadccf6470a663cb
shinseongcheol <cher3725@naver.com>
2025-01-17 14:03:40 +0900

    Add sequelize model picture

---
+++ backend/models/picture.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "picture_tb",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        comment: "사진 고유 ID",
+      },
+      original_name: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+        comment: "오리지널 사진 명",
+      },
+      encoding: {
+        type: DataTypes.STRING(8),
+        allowNull: false,
+        comment: "인코딩 방식",
+      },
+      mime_type: {
+        type: DataTypes.STRING(16),
+        allowNull: false,
+        comment: "MIME 타입",
+      },
+      size: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        comment: "크기"
+      },
+      destination: {
+        type: DataTypes.STRING(256),
+        allowNull: false,
+        comment: "저장 위치"
+      },
+      filename: {
+        type: DataTypes.STRING(256),
+        allowNull: false,
+        comment: "저장된 사진 명"
+      }
+    },
+    {
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+};
