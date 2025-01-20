58f38d6b059c10d6401016eb7d7e9fd8ae534e04
shinseongcheol <cher3725@naver.com>
2025-01-20 23:59:53 +0900

    Feat "axios 추가, 카카오 로그인 인가 코드"

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,25 +1,27 @@
-const db = require("../models");
-const Picture = db.Picture;
+const axios = require('axios');
+require('dotenv').config()
+const url = require('url');
 
-const get = async (req, res, next) => {
-    const picture_list = await Picture.findAll();
-    res.send(picture_list);
-};
+const kakao_login = (req, res, next) => {
+    const kakao_oauth_authorize = process.env.KAKAO_OAUTH_AUTHORIZE;
+    const kakao_client_id = process.env.KAKAO_CLIENT_ID;
+    const kakao_redirect_uri = process.env.KAKAO_REDIRECT_URI;
+    const kakao_response_type = process.env.KAKAO_RESPONSE_TYPE;
 
-const post = async (req, res, next) => {
-    const file = req.file;
-    const picture = await Picture.create({
-        original_name: file.originalname,
-        encoding: file.encoding,
-        mime_type: file.mimetype,
-        size: file.size,
-        destination: file.destination,
-        filename: file.filename,
-    });
-    res.send(picture);
-};
+    const url = new URL(kakao_oauth_authorize);
+    url.searchParams.append('client_id', kakao_client_id);
+    url.searchParams.append('redirect_uri', kakao_redirect_uri);
+    url.searchParams.append('response_type', kakao_response_type);
+    res.setHeader('Access-Control-Allow-origin', 'http://localhost:5174');
+    res.setHeader('Access-Control-Allow-origin', 'http://localhost:5173');
+    res.send(url);
+}
+
+const kakao_authorize_code = (req,res,next) => {
+    console.log(req);
+}
 
 module.exports = {
-    get,
-    post,
+    kakao_login,
+    kakao_authorize_code
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/user.controller');
 
-const path = require('path');
-const multer = require('multer');
-const upload = multer({ dest: path.join(__dirname, '../public/images') })
-
-router.get('/', userController.get)
-router.post('/', upload.single('image'), userController.post)
-
+router.get('/kakao_login', userController.kakao_login)
+router.get('/kakao_authorize_code', userController.kakao_authorize_code)
+router.get('/', (req,res,next) => {
+    
+})
 module.exports = router;
