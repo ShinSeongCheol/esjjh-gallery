58c2e8c183df8ff7e9c6bb16bcfdb77aa7a39b00
shinseongcheol <cher3725@naver.com>
2025-02-02 04:06:48 +0900

    카카오 로그인 구현

--- backend/controllers/kakao.controller.js
+++ backend/controllers/kakao.controller.js
@@ -1,7 +1,7 @@
 const kakaoService = require('../services/kakao.service');
 
 const login = (req, res, next) => {
-    const kakao_oauth_authorize_url = kakaoService.getCode();
+    const kakao_oauth_authorize_url = kakaoService.getKakaoOauthAuthorizeUrl();
     res.setHeader('Access-Control-Allow-origin', process.env.ACCESS_CONTROL_ALLOW_ORIGIN);
     res.redirect(kakao_oauth_authorize_url);
 }
@@ -9,9 +9,7 @@ const login = (req, res, next) => {
 const getCode = async (req, res, next) => {
     const query = req.query;
     const code = query.code;
-    const token_data = await kakaoService.getToken(code);
-    const data = await kakaoService.getUserInfo(token_data);
-    console.log(data);
+    const data = await kakaoService.login(code);
     res.redirect(process.env.FRONT_URL);
 }
 
--- backend/services/kakao.service.js
+++ backend/services/kakao.service.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
+const db = require('../models');
+const { where } = require('sequelize');
+const KakaoUser = db.KakaoUser;
+const KakaoToken = db.KakaoToken;
 
 const kakaoService = {};
-kakaoService.getCode = function() {
+kakaoService.getKakaoOauthAuthorizeUrl = function() {
     const kakao_oauth_authorize_url = process.env.KAKAO_OAUTH_AUTHORIZE_URL;
     const kakao_client_id = process.env.KAKAO_CLIENT_ID;
     const kakao_code_redirect_uri = process.env.KAKAO_CODE_REDIRECT_URI;
@@ -19,10 +23,9 @@ kakaoService.getToken = async function(code) {
     const kakao_oauth_token_url = process.env.KAKAO_OAUTH_TOKEN_URL;
     const kakao_grant_type = process.env.KAKAO_GRANT_TYPE;
     const kakao_client_id = process.env.KAKAO_CLIENT_ID;
-    // const kakao_token_redirect_uri = process.env.KAKAO_TOKEN_REDIRECT_URI;
     const kakao_token_redirect_uri = process.env.KAKAO_CODE_REDIRECT_URI;
 
-    const token_data = await axios.post(kakao_oauth_token_url, {
+    const kakao_token = await axios.post(kakao_oauth_token_url, {
         'grant_type': kakao_grant_type,
         'client_id': kakao_client_id,
         'redirect_uri': kakao_token_redirect_uri,
@@ -36,18 +39,14 @@ kakaoService.getToken = async function(code) {
         console.log(err);
     });
 
-    return token_data;
+    return kakao_token;
 }
 
-kakaoService.getUserInfo = async function(token_data) {
+kakaoService.getUserInfo = async function(kakao_token) {
     const kakao_user_info_url = process.env.KAKAO_USER_INFO_URL;
 
-    const access_token = token_data.access_token;
-    const token_type = token_data.token_type;
-    const refresh_token = token_data.refresh_token;
-    const expires_in = token_data.expires_in;
-    const scope = token_data.scope;
-    const refresh_token_expires_in = token_data.refresh_token_expires_in;
+    const access_token = kakao_token.access_token;
+    const token_type = kakao_token.token_type;
 
     const user_info = await axios.get(kakao_user_info_url, {
         headers: {
@@ -55,25 +54,75 @@ kakaoService.getUserInfo = async function(token_data) {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
         }
     }).then((res) => {
-        console.log(res);
         return res.data;
     })
 
-    const user_dto = {
-        id: user_info.id,
-        nickname: user_info.kakao_account.profile.nickname,
-        thumbnail_image_url: user_info.kakao_account.profile.thumbnail_image_url,
-        profile_image_url: user_info.kakao_account.profile.profile_image_url,
+    return user_info;
+}
 
-        access_token,
-        token_type,
-        refresh_token,
-        expires_in,
-        scope,
-        refresh_token_expires_in,
+kakaoService.login = async (code) => {
+    const token = await kakaoService.getToken(code);
+    const user_info = await kakaoService.getUserInfo(token);
+
+    // 등록된 회원 있는지 확인
+    const kakao_user = await KakaoUser.count({
+        where: {
+            id: user_info.id,
+        }
+    });
+
+    // 회원이 없다면 생성
+    if (kakao_user === 0) {
+        KakaoUser.create({
+            id: user_info.id,
+            nickname: user_info.kakao_account.profile.nickname,
+            thumbnail_image_url: user_info.kakao_account.profile.thumbnail_image_url,
+            profile_image_url: user_info.kakao_account.profile.profile_image_url,
+        });
+    
+        KakaoToken.create({
+            kakao_user_id: user_info.id,
+            token_type: token.token_type,
+            access_token: token.access_token,
+            expires_in: token.expires_in,
+            refresh_token: token.refresh_token,
+            refresh_token_expires_in: token.refresh_token_expires_in,
+            scope: token.scope,
+        });
+    }
+    // 회원이 있다면 업데이트
+    else { 
+        KakaoUser.update(
+            {
+                nickname: user_info.kakao_account.profile.nickname,
+                thumbnail_image_url: user_info.kakao_account.profile.thumbnail_image_url,
+                profile_image_url: user_info.kakao_account.profile.profile_image_url,
+            },
+            {
+                where: {
+                    id: user_info.id,
+                }
+            }
+        )
+
+        KakaoToken.update(
+            {
+                token_type: token.token_type,
+                access_token: token.access_token,
+                expires_in: token.expires_in,
+                refresh_token: token.refresh_token,
+                refresh_token_expires_in: token.refresh_token_expires_in,
+                scope: token.scope,
+            },
+            {
+                where: {
+                    kakao_user_id: user_info.id,
+                }
+            }
+        )
     }
 
-    return user_dto;
+    return user_info;
 }
 
 kakaoService.logout = async (token_data) => {
