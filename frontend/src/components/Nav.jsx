868d0bfd0c00bb500ff311222b0577010875085c
shinseongcheol <cher3725@naver.com>
2025-02-02 15:35:02 +0900

    Nav component 추가

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,16 +1,27 @@
 import { Container, Navbar, Image } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Nav() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const signupButtonClick = () => {
+        navigate('/signin');
+        // let kakao_login_url = import.meta.env.VITE_BACKEND_URL + '/kakao/login'; 
+        // window.open(kakao_login_url, "_parent");
+    }
+
     return(
         <Navbar bg="white" data-bs-theme="white" className='shadow-sm sticky-top'>
             <Container>
-            <Navbar.Brand href="#"><Image src='/logo/esjjh.png' width={150}></Image></Navbar.Brand>
-            <button className='border border-none' onClick={() => {
-                let kakao_login_url = import.meta.env.VITE_BACKEND_URL + '/kakao/login'; 
-                window.open(kakao_login_url, "_parent");
-            }}>
-                <Image src={'/button/kakao_login_small.png'}></Image>
-            </button>
+                <Navbar.Brand href={location.pathname == '/' ? '#' : '/'}>
+                    <Image src='/logo/esjjh.png' width={150}/>
+                </Navbar.Brand>
+                
+                <button className='btn btn-outline-primary' onClick={signupButtonClick}>
+                    로그인
+                    {/* <Image src={'/button/kakao_login_small.png'}></Image> */}
+                </button>
             </Container>
         </Navbar>
     )
