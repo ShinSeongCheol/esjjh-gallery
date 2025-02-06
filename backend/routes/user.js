7036207cf46f9187c61507fb4d0feeedc4d9b082
shinseongcheol <cher3725@naver.com>
2025-02-07 00:00:45 +0900

    유저 프로파일 저장 경로 변경

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/user.controller');
 
 const multer = require("multer");
 const path = require('path');
-const user_profile_upload = multer({ dest: path.join(__dirname, "../public/images") });
+const user_profile_upload = multer({ dest: path.join(__dirname, "../public/profiles") });
 
 router.get('/login', userController.login);
 router.post('/signup', user_profile_upload.single('profile_image'), userController.signup);
