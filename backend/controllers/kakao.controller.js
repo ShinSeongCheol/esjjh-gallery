e4c068067beba32f3e9ff281318b6062a2dd6168
shinseongcheol <cher3725@naver.com>
2025-02-18 01:13:48 +0900

    Refactor Controller

--- backend/controllers/kakao.controller.js
+++ backend/controllers/kakao.controller.js
@@ -8,7 +8,7 @@ const login = (req, res, next) => {
 const getCode = async (req, res, next) => {
     const query = req.query;
     const code = query.code;
-    const data = await kakaoService.login(code);
+    await kakaoService.login(code);
     res.redirect(process.env.FRONT_URL);
 }
 
