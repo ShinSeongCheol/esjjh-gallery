471f9f99b5ff023bf9afcdd7dd3c6af648e5c8cc
shinseongcheol <cher3725@naver.com>
2025-02-19 01:02:06 +0900

    Feat Redis

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,26 @@ const db = require('./models');
 const sequelize = db.sequelize;
 const cors = require('cors');
 
-sequelize.sync({ 
-  force: false,
-  alter: true
+const redis = require('redis');
+const dotenv = require('dotenv');
+dotenv.config({ path: path.join(__dirname, '.env.redis') });
+
+const redis_client = redis.createClient({
+  url: `redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+  legacyMode: true
+});
+
+redis_client.on('connect', () => {
+  console.info('Redis connected!');
+});
+redis_client.on('error', (err) => {
+  console.error('Redis Client Error', err);
+});
+
+redis_client.connect().then();
+
+sequelize.sync({
+  force: true
   })
   .then(() => {
     console.log('데이터베이스 연결 성공');
