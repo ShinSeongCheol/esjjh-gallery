f65f36718f5efb2c1642c680786ac1dbad2fbd7b
shinseongcheol <cher3725@naver.com>
2025-03-07 03:04:49 +0900

    Fix "헤더"
    - 레이아웃 수정

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,16 +2,18 @@ const Header = () => {
     return (
         <>
             <header className={"w-full fixed top-0 left-0 shadow-md"}>
-                <div className={"flex justify-between px-2 py-1"}>
-                    <div className={"flex items-center"}>
+                <div className={"flex px-2 py-1"}>
+                    <div className={"flex items-center grow"}>
                         <a href="/" className="flex-shrink-0">
                             <img className={"h-12 w-auto"} src="/logo/esjjh.png" alt=""/>
                         </a>
-                        <input className={"mx-1 border-1 w-96 h-8"} type={"text"}></input>
-                        <button className={"mx-1 border-1 w-20 h-8"} type={"button"}>검색</button>
+                        <div className={"hidden sm:flex"}>
+                            <input className={"min-w-72 mx-1 border-2 border-gray-200 focus:outline-gray-300 rounded-sm"} type={"text"}></input>
+                            <button className={"min-w-16 mx-1 bg-rose-200 border-2 border-rose-300 hover:bg-rose-400 hover:border-rose-500 hover:cursor-pointer rounded-sm"} type={"button"}>검색</button>
+                        </div>
                     </div>
                     <div className={"flex items-center"}>
-                        <button className={"border-1 w-20 h-8"} type={"button"}>로그인</button>
+                        <button className={"bg-gray-200 border-2 border-gray-300 hover:bg-gray-400 hover:border-gray-500 hover:cursor-pointer rounded-sm w-20 h-8"} type={"button"}>로그인</button>
                     </div>
                 </div>
             </header>
