6da3299308b28fd6aad5454e22f2b4110321a8e0
shinseongcheol <cher3725@naver.com>
2025-03-07 03:09:38 +0900

    Feat "헤더"
    - 검색, 로그인 버튼 클릭 이벤트

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,16 @@
+import {useNavigate} from "react-router-dom";
+
 const Header = () => {
+    const navigate = useNavigate();
+
+    const clickSearchButton = () => {
+        console.log("검색 버튼 클릭");
+    }
+
+    const clickLoginButton = () => {
+        navigate("/signin");
+    }
+
     return (
         <>
             <header className={"w-full fixed top-0 left-0 shadow-md"}>
@@ -9,11 +21,11 @@ const Header = () => {
                         </a>
                         <div className={"hidden sm:flex"}>
                             <input className={"min-w-72 mx-1 border-2 border-gray-200 focus:outline-gray-300 rounded-sm"} type={"text"}></input>
-                            <button className={"min-w-16 mx-1 bg-rose-200 border-2 border-rose-300 hover:bg-rose-400 hover:border-rose-500 hover:cursor-pointer rounded-sm"} type={"button"}>검색</button>
+                            <button className={"min-w-16 mx-1 bg-rose-200 border-2 border-rose-300 hover:bg-rose-400 hover:border-rose-500 hover:cursor-pointer rounded-sm"} type={"button"} onClick={clickSearchButton}>검색</button>
                         </div>
                     </div>
                     <div className={"flex items-center"}>
-                        <button className={"bg-gray-200 border-2 border-gray-300 hover:bg-gray-400 hover:border-gray-500 hover:cursor-pointer rounded-sm w-20 h-8"} type={"button"}>로그인</button>
+                        <button className={"bg-gray-200 border-2 border-gray-300 hover:bg-gray-400 hover:border-gray-500 hover:cursor-pointer rounded-sm w-20 h-8"} type={"button"} onClick={clickLoginButton}>로그인</button>
                     </div>
                 </div>
             </header>
